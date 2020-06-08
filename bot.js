f8883bd2b0e8827e8b83422b81e45a9b21036957
Alex Giovannelli <agiovannelli2234@gmail.com>
2020-06-07 22:03:00 -0400

    Remove unnecessary else statement

--- bot.js
+++ bot.js
@@ -10,19 +10,16 @@ client.on('ready', () => {
 client.on('message', msg => {
     if(!msg.author.bot) {
         if(msg.content.length > 180) {
-            var splitMessage = msg.content.split(' ');
-
+            var splitMessageArray = msg.content.split(' ');
             var readableString = '';
             var readableStringsArray = [];
     
-            splitMessage.forEach(function(token) {
+            splitMessageArray.forEach(function(token) {
                 if(readableString.length >= 180) {
                     readableStringsArray.push(readableString);
                     readableString = '';
-                    readableString = readableString.concat(token + ' ');
-                } else {
-                    readableString = readableString.concat(token + ' ');
                 }
+                readableString = readableString.concat(token + ' ');
             });
 
             readableStringsArray.push(readableString);
